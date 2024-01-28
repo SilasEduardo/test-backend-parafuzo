de7db63ae1cfc86158ae19fd28a753f3746b799c
silasEduardo <silasandrade94@gmail.com>
2024-01-28 15:15:17 -0300

    feat :heavy_plus_sign: add docker_file and docker_compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DB_URI),
+  ],
 })
-export class AppModule {}
+export class AppModule { }
